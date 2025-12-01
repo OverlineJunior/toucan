806e058fa6370de3ded85149d199d527d89a85bf
over <franciscosebastianyjunior@hotmail.com>
2025-12-01 17:19:02 -0300

    Remove unused function isPlugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,7 +3,3 @@ import { App } from './app'
 export interface Plugin {
 	build(app: App): void
 }
-
-export function isPlugin(thing: unknown): thing is Plugin {
-	return typeIs(thing, 'table') && typeIs((thing as Plugin).build, 'function')
-}
