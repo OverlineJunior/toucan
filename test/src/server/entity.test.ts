de5340ebf1c607654fc3675f2c65af64edcf574e
over <ov3rline@proton.me>
2026-05-02 20:56:12 -0300

    Add more entity tests (need reviewing)

--- test/src/server/entity.test.ts
+++ test/src/server/entity.test.ts
@@ -1,7 +1,15 @@
-import { Assert, Test } from '@rbxts/lunit'
-import { entity, pair, ChildOf, resolveId } from '@rbxts/toucan'
+import { Assert, BeforeEach, Test } from '@rbxts/lunit'
+import { entity, pair, ChildOf, resolveId, query, Wildcard, Internal, External, Label } from '@rbxts/toucan'
 
+// TODO! Newly added tests have not been reviewed yet. Make sure to check them out and ensure they are correct.
 class EntityTests {
+	@BeforeEach
+	public reset() {
+		query(Wildcard)
+			.filter((e) => !e.has(Internal) && !e.has(External))
+			.forEach((e) => e.despawn())
+	}
+
 	@Test
 	public entityCreation() {
 		const e = entity()
@@ -33,6 +41,12 @@ class EntityTests {
 		Assert.equal(parent.children().size(), 1)
 		Assert.equal(parent.children()[0], child)
 		Assert.equal(child.targetOf(ChildOf), parent)
+		Assert.equal(child.targetsOf(ChildOf).size(), 1)
+		Assert.equal(child.targetsOf(ChildOf)[0], parent)
+		Assert.equal(child.relationships().size(), 1)
+		// TODO! Toucan should support getting the relation and target of relationship pairs.
+		// Assert.equal(child.relationships()[0].relation, ChildOf)
+		// Assert.equal(child.relationships()[0].target, parent)
 	}
 
 	@Test
@@ -41,6 +55,34 @@ class EntityTests {
 		const resolved = resolveId(e.id)
 		Assert.equal(resolved, e)
 		Assert.true(resolved?.exists())
+	}
+
+	@Test
+	public entityRemove() {
+		const e = entity()
+		Assert.true(e.has(Label))
+		e.remove(Label)
+		// TODO! `handle.remove()` should error when trying to remove internal components like `Label`.
+		// ! Consider adding another standard component called `Persistent` that could be given to every internal component.
+		Assert.false(e.has(Label))
+	}
+
+	@Test
+	public entityClear() {
+		const e = entity()
+		Assert.true(e.has(Label))
+		e.clear()
+		// TODO! `handle.clear()` should skip removal of internal components like `Label`.
+		// ! See the `entityRemove()` test for more details.
+		Assert.false(e.has(Label))
+	}
+
+	@Test
+	public entityComponents() {
+		const e = entity()
+		const comps = e.components()
+		Assert.equal(comps.size(), 1)
+		Assert.equal(comps[0], Label)
 	}
 }
 
